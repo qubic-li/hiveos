ad0488e1b29f635ecee871e1c7d134211d761f8a
jtskxx <158655936+jtskxx@users.noreply.github.com>
2024-08-22 21:00:27 +0200

    PreUpdate h-stats.sh
    It/s display fixed
    Next update:
    - GPU COUNT
    - RUNNER DISPLAY

--- h-stats.sh
+++ h-stats.sh
@@ -1,10 +1,19 @@
-# Define a function to calculate the miner version along with GPU and CPU information (if available).
+#!/bin/bash
+
+# Function to calculate the miner version along with GPU and CPU information (if available).
 get_miner_version() {
     local ver="${custom_version}"
 
-    # Append GPU information to 'ver' only if 'gpu_runner' is defined.
+    # Append GPU information to 'ver' only if 'epoh_runner' is defined.
     if [ -n "${epoh_runner}" ]; then
         ver="${ver}, ${epoh_runner}"
+    fi
+
+    # Check for "pplns cpu" or "pplns gpu" in the log and append accordingly.
+    if grep -q "pplns cpu" "$log_name"; then
+        ver="${ver}, PPLNS: CPU"
+    elif grep -q "pplns gpu" "$log_name"; then
+        ver="${ver}, PPLNS: GPU"
     fi
 
     # Append GPU information to 'ver' only if 'gpu_runner' is defined.
@@ -20,7 +29,7 @@ get_miner_version() {
     echo "$ver"
 }
 
-# This function calculates the uptime of the miner by determining the time elapsed since the last modification of the log file.
+# Function to calculate the uptime of the miner by determining the time elapsed since the last modification of the log file.
 get_miner_uptime(){
   local uptime=0
   local log_time=$(stat --format='%Y' "$log_name")
@@ -39,24 +48,27 @@ get_miner_uptime(){
   echo $uptime
 }
 
-# Function to get log time difference
+# Function to get log time difference.
 get_log_time_diff() {
     local a=0
     let a=$(date +%s)-$(stat --format='%Y' "$log_name")
     echo $a
 }
 
+# File paths and variables
 log_basename="/var/log/miner/custom/custom"
 log_name="$log_basename.log"
 log_head_name="${log_basename}_head.log"
 cpu_conf_name="/hive/miners/custom/qubminer/cpu/appsettings.json"
 gpu_conf_name="/hive/miners/custom/qubminer/gpu/appsettings.json"
 
+# Parsing the log file to retrieve version and runner information
 custom_version=$(grep -Po "(?<=Starting Client ).*" "$log_name" | tail -n1)
 gpu_runner=$(grep -Po "(?<=cuda version ).*(?= is)" "$log_name" | tail -n1 || grep -Po "(?<=hip version ).*(?= is)" "$log_name" | tail -n1)
 cpu_runner=$(grep -Po "(?<=cpu version ).*(?= is)" "$log_name" | tail -n1)
 epoh_runner=$(grep -Po "E:\d+" "$log_name" | tail -n1)
 
+# Calculate the log time difference
 diffTime=$(get_log_time_diff)
 maxDelay=300
 
@@ -69,28 +81,28 @@ if [ "$diffTime" -lt "$maxDelay" ]; then
   [[ $uptime -lt 60 ]] && head -n 150 $log_name > $log_head_name
 
   # Calculating CPU and GPU count
-  cpu_count=`cat $log_head_name | grep "threads are used" | tail -n 1 | cut -d " " -f4`
+  cpu_count=$(grep "threads are used" "$log_head_name" | tail -n 1 | awk '{print $4}')
   if [ -z "$cpu_count" ] && tail -n 1 "$log_head_name" | grep -q "Your Alias is .*-cpu"; then
     cpu_count=1
   else
     cpu_count=0
   fi
 
-  gpu_count=$(cat "$log_head_name" | grep -E "CUDA devices are used|ROCM devices are used" | tail -n 1 | cut -d " " -f4)
+  gpu_count=$(grep -E "CUDA devices are used|ROCM devices are used" "$log_head_name" | tail -n 1 | awk '{print $4}')
   [[ -z $gpu_count ]] && gpu_count=0
   
   if [ $cpu_count -eq 0 ] || [ $gpu_count -eq 0 ]; then
     echo "..."
-    cat $log_name | grep -E "threads are used|CUDA devices are used|ROCM devices are used|Your Alias is .*-cpu" | tail -n 100 > $log_head_name
+    grep -E "threads are used|CUDA devices are used|ROCM devices are used|Your Alias is .*-cpu" "$log_name" | tail -n 100 > $log_head_name
 
-    cpu_count=`cat $log_head_name | grep "threads are used" | tail -n 1 | cut -d " " -f4`
+    cpu_count=$(grep "threads are used" "$log_head_name" | tail -n 1 | awk '{print $4}')
     if [ -z "$cpu_count" ] && tail -n 1 "$log_head_name" | grep -q "Your Alias is .*-cpu"; then
       cpu_count=1
     else
       cpu_count=0
     fi
 
-    gpu_count=$(cat "$log_head_name" | grep -E "CUDA devices are used|ROCM devices are used" | tail -n 1 | cut -d " " -f4)
+    gpu_count=$(grep -E "CUDA devices are used|ROCM devices are used" "$log_head_name" | tail -n 1 | awk '{print $4}')
     [[ -z $gpu_count ]] && gpu_count=0
   fi
   
@@ -105,9 +117,9 @@ if [ "$diffTime" -lt "$maxDelay" ]; then
   echo ----------
 
   if [[ $gpu_count -ge 0 ]]; then
-    gpu_hs=`cat $log_name | tail -n 50 | grep "^GPU" | grep "Try " | tail -n 1 | cut -d " " -f15`
-    gpu_found=`cat $log_name | tail -n 50 | grep "^GPU" | grep "Try " | tail -n 1 | cut -d " " -f6 | cut -d "/" -f1`
-    gpu_submit=`cat $log_name | tail -n 50 | grep "^GPU" | grep "Try " | tail -n 1 | cut -d " " -f6 | cut -d "/" -f2`
+    gpu_hs=$(grep "^GPU" "$log_name" | tail -n 50 | grep -oP "\d+ it/s" | tail -n 1 | awk '{print $1}')
+    gpu_found=$(grep "^GPU" "$log_name" | tail -n 50 | awk -F" | " '{print $2}' | awk '{print $1}' | cut -d "/" -f1)
+    gpu_submit=$(grep "^GPU" "$log_name" | tail -n 50 | awk -F" | " '{print $2}' | awk '{print $1}' | cut -d "/" -f2)
     gpu_temp=$(jq '.temp' <<< "$gpu_stats")
     gpu_fan=$(jq '.fan' <<< "$gpu_stats")
     gpu_bus=$(jq '.busids' <<< "$gpu_stats")
@@ -119,13 +131,13 @@ if [ "$diffTime" -lt "$maxDelay" ]; then
     let gpu_hs_tot=0
 
     for (( i=0; i < ${gpu_count}; i++ )); do
-      hs[$i]=`cat $log_name | tail -n 100 | grep "^GPU" | grep "GPU#$i" | grep "iters/sec" | tail -n 1 | cut -d ":" -f6 | cut -d " " -f2`
+      hs[$i]=$(grep "^GPU#$i" "$log_name" | tail -n 100 | grep -oP "\d+ it/s" | tail -n 1 | awk '{print $1}')
       [[ -z ${hs[$i]} ]] && hs[$i]=0
       let gpu_hs_tot=$gpu_hs_tot+${hs[$i]}
       temp[$i]=$(jq .[$i] <<< "$gpu_temp")
       fan[$i]=$(jq .[$i] <<< "$gpu_fan")
       busid=$(jq .[$i] <<< "$gpu_bus")
-      bus_numbers[$i]=`echo $busid | cut -d ":" -f1 | cut -c2- | awk -F: '{ printf "%d\n",("0x"$1) }'`
+      bus_numbers[$i]=$(echo $busid | cut -d ":" -f1 | cut -c2- | awk -F: '{ printf "%d\n",("0x"$1) }')
     done
     if [[ $gpu_hs_tot -eq 0 ]]; then
       for (( i=0; i < ${gpu_count}; i++ )); do
@@ -136,15 +148,15 @@ if [ "$diffTime" -lt "$maxDelay" ]; then
     fi
     if [[ $gpu_hs_tot -eq 0 ]]; then
       for (( i=0; i < ${gpu_count}; i++ )); do
-        hs[$i]=`printf "%.1f\n" $((10 * $gpu_hs / $gpu_count))e-1`
+        hs[$i]=$(printf "%.1f\n" $((10 * $gpu_hs / $gpu_count))e-1)
       done
     fi
   fi
   if [[ $cpu_count -ge 0 ]]; then
-    cpu_hs=`cat $log_name | tail -n 50 | grep "^CPU" | grep "Try " | tail -n 1 | cut -d " " -f15`
-    cpu_found=`cat $log_name | tail -n 50 | grep "^CPU" | grep "Try " | tail -n 1 | cut -d " " -f6 | cut -d "/" -f1`
-    cpu_submit=`cat $log_name | tail -n 50 | grep "^CPU" | grep "Try " | tail -n 1 | cut -d " " -f6 | cut -d "/" -f2`
-    hs[$gpu_count]=`cat $log_name | tail -n 50 | grep "^CPU" | grep "Try " | tail -n 1 | cut -d " " -f15`
+    cpu_hs=$(grep "^CPU" "$log_name" | tail -n 50 | grep -oP "\d+ it/s" | tail -n 1 | awk '{print $1}')
+    cpu_found=$(grep "^CPU" "$log_name" | tail -n 50 | awk -F" | " '{print $2}' | awk '{print $1}' | cut -d "/" -f1)
+    cpu_submit=$(grep "^CPU" "$log_name" | tail -n 50 | awk -F" | " '{print $2}' | awk '{print $1}' | cut -d "/" -f2)
+    hs[$gpu_count]=$(grep "^CPU" "$log_name" | tail -n 50 | grep -oP "\d+ it/s" | tail -n 1 | awk '{print $1}')
     temp[$gpu_count]="$cpu_temp"
     fan[$gpu_count]=""
     bus_numbers[$gpu_count]="null"
@@ -161,20 +173,19 @@ if [ "$diffTime" -lt "$maxDelay" ]; then
   let ac=$gpu_found+$cpu_found
   let rj=$gpu_submit+$cpu_submit-$ac
 
-  khs=`echo $khs | awk '{print $1/1000}'`
-
+  khs=$(echo $khs | awk '{print $1/1000}')
 
   stats=$(jq -nc \
             --arg khs "$khs" \
             --arg hs_units "$hs_units" \
-            --argjson hs "`echo ${hs[@]} | tr " " "\n" | jq -cs '.'`" \
-            --argjson temp "`echo ${temp[@]} | tr " " "\n" | jq -cs '.'`" \
-            --argjson fan "`echo ${fan[@]} | tr " " "\n" | jq -cs '.'`" \
+            --argjson hs "$(echo ${hs[@]} | tr " " "\n" | jq -cs '.')" \
+            --argjson temp "$(echo ${temp[@]} | tr " " "\n" | jq -cs '.')" \
+            --argjson fan "$(echo ${fan[@]} | tr " " "\n" | jq -cs '.')" \
             --arg uptime "$uptime" \
             --arg ver "$ver" \
             --arg ac "$ac" --arg rj "$rj" \
             --arg algo "$algo" \
-            --argjson bus_numbers "`echo ${bus_numbers[@]} | tr " " "\n" | jq -cs '.'`" \
+            --argjson bus_numbers "$(echo ${bus_numbers[@]} | tr " " "\n" | jq -cs '.')" \
             '{$hs, $hs_units, $temp, $fan, $uptime, $ver, ar: [$ac, $rj], $algo, $bus_numbers}')
 
 else
