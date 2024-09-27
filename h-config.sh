2d953d7820901d9c6b0d98b6d70191d5316f14a1
JETSKI <158655936+jtskxx@users.noreply.github.com>
2024-09-27 19:32:03 +0200

    Update h-config.sh

--- h-config.sh
+++ h-config.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 process_user_config() {
     while IFS= read -r line; do
         [[ -z $line ]] && continue
@@ -9,31 +11,31 @@ process_user_config() {
             # Local file
             LOCAL_FILE="/hive/miners/custom/downloads/qubminer-latest.tar.gz"
 
-			if [ -e $LOCAL_FILE ]; then
+            # URL of the remote file and its hash
+            REMOTE_FILE_URL="https://github.com/qubic-li/hiveos/releases/download/latest/qubminer-latest.tar.gz"
+            REMOTE_HASH_URL="https://github.com/qubic-li/hiveos/releases/download/latest/qubminer-latest.hash"
 
-				# URL of the remote file and its hash
-				REMOTE_FILE_URL="https://github.com/qubic-li/hiveos/releases/download/latest/qubminer-latest.tar.gz"
-				REMOTE_HASH_URL="https://github.com/qubic-li/hiveos/releases/download/latest/qubminer-latest.hash"
+            # Check the availability of the remote hash
+            if curl --output /dev/null --silent --head --fail "$REMOTE_HASH_URL"; then
+                # Download the remote hash
+                REMOTE_HASH=$(curl -s -L "$REMOTE_HASH_URL")
 
-				# Check the availability of the remote hash
-				if curl --output /dev/null --silent --head --fail "$REMOTE_HASH_URL"; then
-					# Download the remote hash
-					REMOTE_HASH=$(curl -s -L "$REMOTE_HASH_URL")
+                # Calculate the SHA256 hash of the local file
+                LOCAL_HASH=$(sha256sum "$LOCAL_FILE" | awk '{print $1}')
 
-					# Calculate the SHA256 hash of the local file
-					LOCAL_HASH=$(sha256sum "$LOCAL_FILE" | awk '{print $1}')
-
-					# Compare the hashes
-					if [ "$LOCAL_HASH" != "$REMOTE_HASH" ]; then
-						echo "Hashes of local and remote ($REMOTE_FILE_URL) miners are different. Removing the local file of the miner and restarting the miner. In case you see the error multiple times, check the URL of the miner in the Flight Sheet."
-						# Remove old local miner and restart the miner
-						rm "$LOCAL_FILE"
-						echo "Miner restarting in 10 sec..."
-						screen -d -m miner restart
-					fi
-				fi
-			fi
+                # Compare the hashes
+                if [ "$LOCAL_HASH" != "$REMOTE_HASH" ]; then
+                    echo "Hashes of local and remote ($REMOTE_FILE_URL) miners are different. Removing the local file of the miner and restarting the miner. In case you see the error multiple times, check the URL of the miner in the Flight Sheet."
+                    # Remove old local miner and restart the miner
+                    rm "$LOCAL_FILE"
+                    echo "Miner restarting in 10 sec..."
+                    screen -d -m miner restart
+                fi
+            fi
         else
+            # Remove spaces only from the beginning of the line
+            line=$(echo "$line" | sed 's/^[[:space:]]*//')
+
             # Extract parameter and its value from the configuration line
             param=$(awk -F':' '{gsub(/\"/, ""); print $1}' <<< "$line")
             value=$(awk -F':' '{gsub(/^[[:space:]]*/, ""); print substr($0, length($1) + 2)}' <<< "$line")
@@ -44,15 +46,14 @@ process_user_config() {
             # Perform replacements in the parameter
             modified_param=$(echo "$param_high" | awk '{
                 gsub("PAYOUTID", "payoutId");
-                gsub("AMOUNTOFTHREADS", "amountOfThreads");
+                gsub("CPUTHREADS", "cpuThreads");
                 gsub("ACCESSTOKEN", "accessToken");
                 gsub("ALLOWHWINFOCOLLECT", "allowHwInfoCollect");
                 gsub("HUGEPAGES", "hugePages");
-                gsub("CPUONLY", "cpuOnly");
                 gsub("ALIAS", "alias");
                 gsub("OVERWRITES", "overwrites");
                 gsub("IDLESETTINGS", "idleSettings");
-                gsub("ISPPS=", "\"isPps\": ");
+                gsub("PPS=", "\"pps\": ");
                 gsub("USELIVECONNECTION", "useLiveConnection");
                 gsub("TRAINER", "trainer");
                 print $0;
@@ -60,17 +61,6 @@ process_user_config() {
 
             # Check if modifications were made, if not, use the original parameter
             [[ "$param" != "$modified_param" ]] && param=$modified_param
-
-            # Handle cpuOnly parameter with additional 'yes' and '"yes"' options
-            if [[ "$param" == "cpuOnly" ]]; then
-                if [[ "$value" == "\"true\"" || "$value" == "true" || "$value" == "\"yes\"" || "$value" == "yes" ]]; then
-                    Settings=$(jq --arg value "true" '.cpuOnly = $value' <<< "$Settings")
-                    SettingsGpu=$(jq --arg value "true" '.cpuOnly = $value' <<< "$SettingsGpu")
-                else
-                    echo "Invalid value for cpuOnly: $value. It must be '\"true\"', 'true', '\"yes\"', or 'yes'. Skipping this entry."
-                fi
-                continue
-            fi
 
             # General processing for other parameters
             if [[ ! -z "$value" ]]; then
@@ -81,20 +71,36 @@ process_user_config() {
                     gpuOnly=$(jq -r '.gpuOnly // empty' <<< "$value")
                     if [[ "$gpuOnly" == "true" ]]; then
                         value=$(jq 'del(.gpuOnly)' <<< "$value")
-                        SettingsGpu=$(jq --argjson idleSettings "$value" '.idleSettings = $idleSettings' <<< "$SettingsGpu")
+                        SettingsGpu=$(jq --argjson idleSettings "$value" '
+                            .idleSettings = $idleSettings | 
+                            .idleSettings.preCommand = ($idleSettings.preCommand // null) |
+                            .idleSettings.preCommandArguments = ($idleSettings.preCommandArguments // null) |
+                            .idleSettings.command = ($idleSettings.command // null) |
+                            .idleSettings.arguments = ($idleSettings.arguments // null) |
+                            .idleSettings.postCommand = ($idleSettings.postCommand // null) |
+                            .idleSettings.postCommandArguments = ($idleSettings.postCommandArguments // null)
+                        ' <<< "$SettingsGpu")
                     else
-                        Settings=$(jq --argjson idleSettings "$value" '.idleSettings = $idleSettings' <<< "$Settings")
+                        Settings=$(jq --argjson idleSettings "$value" '
+                            .idleSettings = $idleSettings | 
+                            .idleSettings.preCommand = ($idleSettings.preCommand // null) |
+                            .idleSettings.preCommandArguments = ($idleSettings.preCommandArguments // null) |
+                            .idleSettings.command = ($idleSettings.command // null) |
+                            .idleSettings.arguments = ($idleSettings.arguments // null) |
+                            .idleSettings.postCommand = ($idleSettings.postCommand // null) |
+                            .idleSettings.postCommandArguments = ($idleSettings.postCommandArguments // null)
+                        ' <<< "$Settings")
                     fi
                 elif [[ "$param" == "accessToken" ]]; then
                     value=$(echo "$value" | sed 's/^"//;s/"$//')
                     Settings=$(jq --arg value "$value" '.accessToken = $value' <<< "$Settings")
                     SettingsGpu=$(jq --arg value "$value" '.accessToken = $value' <<< "$SettingsGpu")
-                elif [[ "$param" == "isPps" ]]; then
+                elif [[ "$param" == "pps" ]]; then
                     if [[ "$value" == "true" || "$value" == "false" ]]; then
-                        Settings=$(jq --argjson value "$value" '.isPps = $value' <<< "$Settings")
-                        SettingsGpu=$(jq --argjson value "$value" '.isPps = $value' <<< "$SettingsGpu")
+                        Settings=$(jq --argjson value "$value" '.pps = $value' <<< "$Settings")
+                        SettingsGpu=$(jq --argjson value "$value" '.pps = $value' <<< "$SettingsGpu")
                     else
-                        echo "Invalid value for isPps: $value. It must be 'true' or 'false'. Skipping this entry."
+                        echo "Invalid value for pps: $value. It must be 'true' or 'false'. Skipping this entry."
                     fi
                 elif [[ "$param" == "useLiveConnection" ]]; then
                     if [[ "$value" == "true" || "$value" == "false" ]]; then
@@ -104,10 +110,10 @@ process_user_config() {
                         echo "Invalid value for useLiveConnection: $value. It must be 'true' or 'false'. Skipping this entry."
                     fi
                 else
-                    if [[ "$param" == "trainer.cpuVersion" ]]; then
-                        Settings=$(jq --arg value "$value" '.trainer.cpuVersion = $value' <<< "$Settings")
-                    elif [[ "$param" == "trainer.gpuVersion" ]]; then
-                        SettingsGpu=$(jq --arg value "$value" '.trainer.gpuVersion = $value' <<< "$SettingsGpu")
+                    if [[ "$param" == "trainer.cpuThreads" ]]; then
+                        Settings=$(jq --arg value "$value" '.trainer.cpuThreads = ($value | tonumber)' <<< "$Settings")
+                    elif [[ "$param" == "trainer.gpu" ]]; then
+                        SettingsGpu=$(jq --argjson value "$value" '.trainer.gpu = $value' <<< "$SettingsGpu")
                     elif [[ "$value" == "null" ]]; then
                         Settings=$(jq --arg param "$param" '.[$param] = null' <<< "$Settings")
                         SettingsGpu=$(jq --arg param "$param" '.[$param] = null' <<< "$SettingsGpu")
@@ -127,28 +133,29 @@ process_user_config() {
 # Main script logic
 
 # Processing global settings
-Settings=$(jq -r '.Settings' "/hive/miners/custom/$CUSTOM_NAME/appsettings_global.json" | envsubst)
+GlobalSettings=$(jq -r '.ClientSettings' "/hive/miners/custom/$CUSTOM_NAME/appsettings_global.json" | envsubst)
 
-# Initialize GPU settings as well
-SettingsGpu=$Settings
+# Initialize Settings and SettingsGpu
+Settings="$GlobalSettings"
+SettingsGpu="$GlobalSettings"
 
 # Delete old settings
 eval "rm -rf /hive/miners/custom/$CUSTOM_NAME/cpu/appsettings.json"
 eval "rm -rf /hive/miners/custom/$CUSTOM_NAME/gpu/appsettings.json"
 
-# Processing the template
+# Processing the template (alias)
 if [[ ! -z $CUSTOM_TEMPLATE ]]; then
-    Settings=$(jq --null-input --argjson Settings "$Settings" --arg alias "$CUSTOM_TEMPLATE" '$Settings + {$alias}')
-    SettingsGpu=$(jq --null-input --argjson SettingsGpu "$SettingsGpu" --arg alias "$CUSTOM_TEMPLATE" '$SettingsGpu + {$alias}')
+    Settings=$(jq --arg alias "$CUSTOM_TEMPLATE" '.alias = $alias' <<< "$Settings")
+    SettingsGpu=$(jq --arg alias "$CUSTOM_TEMPLATE" '.alias = $alias' <<< "$SettingsGpu")
 fi
 
 # Processing user configuration
 [[ ! -z $CUSTOM_USER_CONFIG ]] && process_user_config
 
-# Adding URL settings
+# Adding poolAddress settings
 if [[ ! -z $CUSTOM_URL ]]; then
-    Settings=$(jq --null-input --argjson Settings "$Settings" --arg baseUrl "$CUSTOM_URL" '$Settings + {$baseUrl}')
-    SettingsGpu=$(jq --null-input --argjson SettingsGpu "$SettingsGpu" --arg baseUrl "$CUSTOM_URL" '$SettingsGpu + {$baseUrl}')
+    Settings=$(jq --arg poolAddress "$CUSTOM_URL" '.poolAddress = $poolAddress' <<< "$Settings")
+    SettingsGpu=$(jq --arg poolAddress "$CUSTOM_URL" '.poolAddress = $poolAddress' <<< "$SettingsGpu")
 fi
 
 # Check and modify Settings for hugePages parameter
@@ -159,14 +166,20 @@ if [[ $(jq '.hugePages' <<< "$Settings") != null ]]; then
     fi
 fi
 
-# Additional check in the Settings for only CPU mining
-if [[ $(jq '.cpuOnly == "true" or .amountOfThreads != 0' <<< "$Settings") == true ]]; then
-    Settings=$(jq '.alias |= . + "-cpu" | .trainer.cpu = true | .trainer.gpu = false | .allowHwInfoCollect = false | del(.overwrites.CUDA)' <<< "$Settings")
-    echo "{\"Settings\":$Settings}" | jq . > "/hive/miners/custom/$CUSTOM_NAME/cpu/appsettings.json"
+# Check and create settings for CPU mining
+if [[ $(jq '.trainer.cpu == true' <<< "$Settings") == true ]]; then
+    Settings=$(jq '.alias |= . + "-cpu" | .trainer.gpu = false | .allowHwInfoCollect = false | del(.overwrites.CUDA)' <<< "$Settings")
+    # Set default cpuThreads to 0 if not specified
+    if [[ $(jq '.trainer.cpuThreads' <<< "$Settings") == null ]]; then
+        Settings=$(jq '.trainer.cpuThreads = 0' <<< "$Settings")
+    fi
+    echo "{\"ClientSettings\":$Settings}" | jq . > "/hive/miners/custom/$CUSTOM_NAME/cpu/appsettings.json"
 fi
 
-# Additional check and modification in the Settings for GPU mining
-if [[ $(jq '.cpuOnly == "true"' <<< "$Settings") == false ]]; then
-    SettingsGpu=$(jq '.alias |= . + "-gpu" | .trainer.cpu = false | .trainer.gpu = true | .amountOfThreads = 0 | del(.hugePages)' <<< "$SettingsGpu")
-    echo "{\"Settings\":$SettingsGpu}" | jq . > "/hive/miners/custom/$CUSTOM_NAME/gpu/appsettings.json"
+# Check and create settings for GPU mining
+if [[ $(jq '.trainer.gpu == true' <<< "$SettingsGpu") == true ]]; then
+    SettingsGpu=$(jq '.alias |= . + "-gpu" | .trainer.cpu = false | del(.trainer.cpuThreads) | del(.hugePages)' <<< "$SettingsGpu")
+    echo "{\"ClientSettings\":$SettingsGpu}" | jq . > "/hive/miners/custom/$CUSTOM_NAME/gpu/appsettings.json"
 fi
+
+echo "Settings created successfully."
